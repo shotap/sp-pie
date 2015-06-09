c360510ded97674d52489a8e3ad0cb1f5d6506ac
Shota <shota.papi@gmail.com>
2015-06-10 01:56:06 +0300

    directive adds svg to the DOM

--- src/js/sp-pie.js
+++ src/js/sp-pie.js
@@ -1,6 +1,5 @@
 /**
  *
- *
  * ███████╗██████╗       ██████╗ ██╗███████╗
  * ██╔════╝██╔══██╗      ██╔══██╗██║██╔════╝
  * ███████╗██████╔╝█████╗██████╔╝██║█████╗
@@ -8,22 +7,32 @@
  * ███████║██║           ██║     ██║███████╗
  * ╚══════╝╚═╝           ╚═╝     ╚═╝╚══════╝
  *
+ *
  * § Version: 0.0.1
  * § Author: Shota Papiashvili
  * § Repo: https://github.com/shotap/sp-pie
  *
  */
 
-
-/**
- *
- *
- */
 angular
-    .module('sp.sp-pie', [])
+    .module('sp-pie', [])
     .directive('spPie', [ function(){
-        return {
+        var tmpl='<svg viewBox="0 0 64 64" class="sp-pie"></svg>';
 
+        return {
+            restrict: 'AE',
+            type: 'svg',
+            replace: 'true',
+            scope: true,
+            template: tmpl,
+            link: function(scope, elem, attr){
+                var circle = angular.element(document.createElementNS('http://www.w3.org/2000/svg', 'circle'))
+                    .attr('r', '25%')
+                    .attr('cx', '50%')
+                    .attr('cy', '50%')
+                    .attr('class', 'sp-pie-slice-1');
+                elem.append(circle);
+            }
         };
     } ])
 ;
