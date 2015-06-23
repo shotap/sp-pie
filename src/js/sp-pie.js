ee764e498f65853b266a4ab5c6f6700b1e57348f
Shota <shota.papi@gmail.com>
2015-06-23 22:19:29 +0300

    animations

--- src/js/sp-pie.js
+++ src/js/sp-pie.js
@@ -14,9 +14,28 @@
  *
  */
 
+angular.module('sp-pie', []);
+
 angular
-    .module('sp-pie', [])
-    .directive('spPie', [ function(){
+    .module('sp-pie')
+    .service('$spSlicer', [function(){
+        return {
+            cutMeASlice: function(slice, total, elem){
+                var ci = angular
+                    .element(document.createElementNS('http://www.w3.org/2000/svg', 'circle'))
+                    .attr('r', '25%').attr('cx', '50%').attr('cy', '50%')
+                    .attr('style', 'fill:transparent;stroke:' + slice.color + ';stroke-width:32;stroke-dasharray:0,' + total + ',' + v.value + ', 100;transform-origin:center;transform:rotate(-90deg);');
+
+                elem.append(ci);
+                return total + slice.value;
+            }
+        }
+    }])
+;
+
+angular
+    .module('sp-pie')
+    .directive('spPie', ['$spSlicer', function($spSlicer){
         var tmpl='<svg viewBox="0 0 64 64" class="sp-pie"></svg>';
 
         return {
@@ -27,19 +46,24 @@ angular
             template: tmpl,
             link: function(scope, elem, attr){
                 elem.attr('style', 'width:' + attr.size + 'px');
-                var slices = scope.$parent[attr.slices];
-                var total = 0;
 
-                angular.forEach(slices, function(v){
-                    var ci = angular
-                        .element(document.createElementNS('http://www.w3.org/2000/svg', 'circle'))
-                        .attr('r', '25%').attr('cx', '50%').attr('cy', '50%')
-                        .attr('style', 'fill:transparent;stroke:' + v.color + ';stroke-width:32;stroke-dasharray:0,'+ total +',' + v.value + ', 100;transform-origin:center;transform:rotate(-90deg);');
+                if (attr.slices) {
+                    var slices = scope.$parent[attr.slices];
+                    var total = 0;
 
-                    elem.append(ci);
-                    total += v.value;
-                }, total);
+                    angular.forEach(slices, function (v) {
+                        total = $spSlicer.cutMeASlice(v, total, elem);
+                    }, total);
+                }
             }
         };
+    }])
+;
+
+
+angular
+    .module('sp-pie')
+    .directive('spSlice', [ function(){
+
     }])
 ;
